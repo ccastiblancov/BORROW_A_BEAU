88a123a7b9ff5ac488aad3b8b3df0cef5c9bda63
Ailin Chen <ailinchenirene@gmail.com>
2023-06-06 15:13:34 +0200

    bookings backend

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,43 @@
 class BookingsController < ApplicationController
+
+  def index
+    @bookings = Bookings.all
+  end
+
+  def new
+    @booking = Bookings.new
+    @booking.user = current_user
+  end
+
+  def create
+    @booking = Bookings.new(booking_params)
+    if @booking.save
+      redirect_to @booking, notice: "Booking has been created."
+    else
+      rendor :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    if @booking.update(booking_params)
+      redirect_to @booking, notice: "Booking is saved."
+    else
+      rendor :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to bookings_url, notice: "Booking has been deleted.", status: :see_other
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :profile_id)
+  end
 end
