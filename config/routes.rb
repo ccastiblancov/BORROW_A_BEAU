69c0d7361c81b022bd85ed3167e629bc0bbbc088
Ailin Chen <ailinchenirene@gmail.com>
2023-06-06 17:29:12 +0200

    add another line

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,8 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+
   # Defines the root path route ("/")
   # root "articles#index"
   resources :bookings
-<<<<<<< HEAD
-=======
-
->>>>>>> 713fa771741ed913d80d1eb91900850ac3489d70
 end
